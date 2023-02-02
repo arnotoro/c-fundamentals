d8434a479eac0cee43abb962e7b009e9e940cbe2
arnotoro <arno.toro@gmail.com>
2023-02-02 16:07:44 +0200

    completed week3

--- L3/L3T5.c
+++ L3/L3T5.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 #define MAX 30
 
 
 void setSeed();
 int getAmount();
-int generateCharacters(int amount);
-int checkForChar();
+char *generateCharacters(char *characters, int amount);
+void checkForChar(char *characters);
 
 int main(void) {
     int amount = 0;
+    int length = 0;
+    int foundC = 0;
+    char characters[MAX] = {0};
+    char *ptr;
 
     printf("Tämä ohjelma luo satunnaisia merkkijonoja.\n");
 
     setSeed();
     amount = getAmount();
 
-    generateCharacters(amount);
+    ptr = generateCharacters(characters, amount);
+    checkForChar(ptr);
 
+
+    printf("Generoitiin merkkijono '%s', jossa on %d merkkiä.\n", ptr, strlen(ptr));
     printf("Kiitos ohjelman käytöstä.\n");
     return 0;
 }
@@ -40,20 +49,41 @@ int getAmount() {
     return amount;
 }
 
-int generateCharacters(int amount) {
+char *generateCharacters(char *characters, int amount) {
     int i = 0;
     int iArvo = 0;
-    char characters[MAX];
     char cMerkki;
 
     for (i = 0; i < amount; i++) {
         iArvo = rand() % 95 + 32;
         cMerkki = iArvo;
-        characters[i] = cMerkki;
+        
+        if ((isalpha(cMerkki) || isspace(cMerkki) || isdigit(cMerkki))) {
+            characters[i] = cMerkki;
+        } else {
+            i--; amount--;
+        }
     }
 
-    printf("%s", characters);
-
-    return 0;
+    return characters;
 }
 
+void checkForChar(char *characters) {
+    int i = 0;
+    int foundC = 0;
+
+    while (characters[i] != '\0') {
+        if (characters[i] == 'c') {
+            foundC = 1;
+        }
+        i++;
+    }
+
+    if (foundC) {
+        printf("\nMerkkijonosta löydettiin merkki 'c'.\n");
+    } else {
+        printf("\nMerkkijonossa ei ollut merkkiä 'c'.\n");
+    }
+
+    return;
+}
