35e21e8e55f62f7108da9d260e9fa472faceaa99
arnotoro <arno.toro@gmail.com>
2023-02-02 12:50:12 +0200

    advanced week3

--- L3/L3T4.c
+++ L3/L3T4.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
-#define MAX 30
+#include <stdlib.h>
+#define MAX 60
 
 int merkkijonoPituus(char *merkkiJono);
-char *kopioMerkkijono(char *uusi, char *vanha);
+char *kopioMerkkijono(char *merkkiJono1, char *merkkiJono2);
+
+int main(void) {
+    char merkkiJono1[MAX], merkkiJono2[MAX];
+    char *ptr;
+
+    printf("Anna ensimmäinen merkkijono (max 30 merkkiä): ");
+    fgets(merkkiJono1, MAX, stdin);
+    printf("Anna toinen merkkijono (max 30 merkkiä): ");
+    fgets(merkkiJono2, MAX, stdin);
+
+    merkkiJono1[merkkijonoPituus(merkkiJono1)-1] = '\0';
+    merkkiJono2[merkkijonoPituus(merkkiJono2)-1] = '\0';
+
+    ptr = kopioMerkkijono(merkkiJono1, merkkiJono2);
+
+    if (ptr != 0) {
+        printf("Yhdistetty merkkijono on:\n'%s'\n", ptr);
+    }
+
+    printf("Kiitos ohjelman käytöstä.\n");
+    return 0;
+}
 
 int merkkijonoPituus(char *merkkiJono) {
     int pituus = 0;
@@ -14,43 +37,26 @@ int merkkijonoPituus(char *merkkiJono) {
         pituus++;
         ptr++;
     }
+
     return pituus;
 }
 
-char *kopioMerkkijono(char *uusi, char *vanha) {
-    char *uusiPtr;
+char *kopioMerkkijono(char *merkkiJono1, char *merkkiJono2) {
     int i = 0;
+    int pituus1 = merkkijonoPituus(merkkiJono1);
+    int pituus2 = merkkijonoPituus(merkkiJono2);
 
-    while(vanha[i] != '\0') {
-        uusi[i] = vanha[i];
+    if ((pituus1+pituus2) > MAX) {
+        printf("Merkkijonojen yhteenlaskettu pituus on liian pitkä ohjelman muistialueelle.\n");
+        return 0;
+    }
+    
+    while(merkkiJono2[i] != '\0') {
+        merkkiJono1[pituus1 + i] = merkkiJono2[i];
         i++;
     }
-    uusi[i] = '\0';
-    uusiPtr = uusi;
-    return uusiPtr;
-}
 
+    merkkiJono1[pituus1 + i] = '\0';
 
-
-int main(void) {
-    char merkkijono1[MAX];
-    char merkkijono2[MAX];
-    int pituus1;
-    int pituus2;
-    int pituus3;
-    char *merkkijono2_ptr;
-
-    printf("Anna kopioitava merkkijono: ");
-    fgets(merkkijono1, 28, stdin);
-
-    pituus1 = merkkijonoPituus(merkkijono1);
-    merkkijono1[pituus1 - 1] = '\0'; //otetaan huomioon rivinvaihto
-    merkkijono2_ptr = kopioMerkkijono(merkkijono2, merkkijono1);
-    pituus2 = merkkijonoPituus(merkkijono2_ptr);
-
-    printf("Merkkijono 1 on '%s'.\n", merkkijono1);
-    printf("Merkkijono 2 on '%s'.\n", merkkijono2_ptr);
-
-    printf("Kiitos ohjelman käytöstä.\n");
-    return 0;
+    return merkkiJono1;
 }
--- L3/L3T5.c
+++ L3/L3T5.c
@@ -1,69 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 48
+#define MAX 30
 
 
-int kirjoitaTiedosto(char *tiedostonNimi, int alustus);
-int lueTiedosto(char *tiedostonNimi);
-
-int kirjoitaTiedosto(char *tiedostonNimi, int alustus) {
-    int i;
-    int r;
-    FILE *tiedosto;
-
-    srand(alustus);
-
-    if ((tiedosto = fopen(tiedostonNimi, "wb")) == NULL) {
-        perror("Tiedoston avaaminen epäonnistui, lopetetaan");
-        exit(0);
-    }
-
-    for(i = 0; i < 20; i++) {
-        r = (rand() % (999+1-0)+0);
-        fwrite(&r, sizeof(r), 1, tiedosto);
-    }
-    fclose(tiedosto);
-    return 0;
-}
-
-int lueTiedosto(char *tiedostonNimi) {
-    FILE *tiedosto;
-    int luku;
-    int i;
-
-    if ((tiedosto = fopen(tiedostonNimi, "rb")) == NULL) {
-        perror("Tiedoston avaaminen epäonnistui, lopetetaan:");
-        exit(0);
-    }    
-
-    printf("Tiedostossa on seuraavat luvut:\n");
-    for(i = 0; i<20; i++) {
-        fread(&luku, sizeof(luku), 1, tiedosto);
-        printf("%d ", luku);
-    }
-    fclose(tiedosto);
-    return 0;
-}
-
-
-
+void setSeed();
+int getAmount();
+int generateCharacters(int amount);
+int checkForChar();
 
 int main(void) {
-    char tnimi[MAX];
-    int alkuarvo;
+    int amount = 0;
 
-    printf("Anna käsiteltävän tiedoston nimi: ");
-    scanf("%s", tnimi);
+    printf("Tämä ohjelma luo satunnaisia merkkijonoja.\n");
 
-    printf("Anna satunnaisluvuille lähtöarvo: ");
-    scanf("%d", &alkuarvo);
+    setSeed();
+    amount = getAmount();
 
-    if (kirjoitaTiedosto(tnimi, alkuarvo) == 0) {
-        printf("Tiedoston kirjoitus onnistui.\n");
-    }
-    if (lueTiedosto(tnimi) == 0) {
-        printf("\nTiedoston lukeminen onnistui.\n");
-    }
+    generateCharacters(amount);
+
     printf("Kiitos ohjelman käytöstä.\n");
     return 0;
 }
+
+void setSeed() {
+    int seed = 0;
+
+    printf("Anna satunnaisgeneraattorin siemenluku: ");
+    scanf("%d", &seed);
+
+    srand(seed);
+}
+
+int getAmount() {
+    int amount = 0;
+
+    printf("Anna arvottavien merkkien määrä: ");
+    scanf("%d", &amount);
+
+    return amount;
+}
+
+int generateCharacters(int amount) {
+    int i = 0;
+    int iArvo = 0;
+    char characters[MAX];
+    char cMerkki;
+
+    for (i = 0; i < amount; i++) {
+        iArvo = rand() % 95 + 32;
+        cMerkki = iArvo;
+        characters[i] = cMerkki;
+    }
+
+    printf("%s", characters);
+
+    return 0;
+}
+
